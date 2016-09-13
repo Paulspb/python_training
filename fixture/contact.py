045b4048f82755a191beca308f942c1ab0bcfb8c
Khometa <pavel.khometa@emc.com>
2016-09-13 15:03:12 +0300

    group & contact classes extracted

---
+++ fixture/contact.py
@@ -0,0 +1,114 @@
+
+
+class ContactHelper:
+    # constructor
+    def __init__(self, app):
+        self.app = app
+
+    def open_contact_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
+
+    def create(self, contact):
+        wd = self.app.wd
+        self.open_contact_page()
+        self.create_names(contact.firstname, contact.lastName, contact.midName, contact.nickName)
+        self.create_company_address1(contact.address1, contact.companyName)
+        self.create_telephones_fax(contact.fax, contact.telHome, contact.telMobile, contact.telWork)
+        self.create_emails(contact.email_1, contact.email_2, contact.email_3)
+        self.create_home_page(contact.home_page)
+        self.create_dates()
+        # fill-in dates2
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys("2002")
+        self.create_address2_home2(contact.address_2, contact.home_2, contact.note_2)
+        # submit
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        self.return_to_contact()
+
+    def create_home_page(self, home_page):
+        wd = self.app.wd
+        wd.find_element_by_name("homepage").click()
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(home_page)
+
+    def create_dates(self):
+        wd = self.app.wd
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[4]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[4]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[3]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[3]").click()
+
+    def create_address2_home2(self, address_2, home_2, note_2):
+        wd = self.app.wd
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(address_2)
+        # fill phone 2
+        wd.find_element_by_name("phone2").click()
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys(home_2)
+        # fill notes
+        wd.find_element_by_name("notes").click()
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys(note_2)
+
+    def create_emails(self, email_1, email_2, email_3):
+        wd = self.app.wd
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(email_1)
+        wd.find_element_by_name("email2").click()
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(email_2)
+        wd.find_element_by_name("email3").click()
+        wd.find_element_by_name("email3").clear()
+        wd.find_element_by_name("email3").send_keys(email_3)
+
+    def create_company_address1(self, address1, companyName):
+        wd = self.app.wd
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(companyName)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(address1)
+
+    def create_telephones_fax(self, fax, telHome, telMobile, telWork):
+        wd = self.app.wd
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(telHome)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(telMobile)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(telWork)
+        wd.find_element_by_name("fax").click()
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(fax)
+
+    def create_names(self, firstname, lastNname, midName, nickName):
+        wd = self.app.wd
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(midName)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(lastNname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(nickName)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys("title")
+
+    def return_to_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home").click()
+
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,19 +4,17 @@ import pytest
 from fixture.application import Application
 from model.contact import Contact
 
-
 @pytest.fixture
 def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
-
 def test_add_contact(app):
     app.session.login( username="admin", password="secret")
-    app.create_contact(Contact(firstname="first_name", lastName="Last_name", midName="Mid_name", nickName="nick_name",
-                            companyName="company1", address1="address11", telHome="homet", telMobile="mobileT",
-                            telWork="wrokT", fax="fas", email_1="email11", email_2="email2", email_3="email3",
-                            address_2="address2", home_2="home2", note_2="note2", home_page="homePage"))
+    app.contact.create(Contact(firstname="first_name", lastName="Last_name", midName="Mid_name", nickName="nick_name",
+                               companyName="company1", address1="address11", telHome="homet", telMobile="mobileT",
+                               telWork="wrokT", fax="fas", email_1="email11", email_2="email2", email_3="email3",
+                               address_2="address2", home_2="home2", note_2="note2", home_page="homePage"))
     app.session.logout()
 
