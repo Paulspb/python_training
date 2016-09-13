868e2ff95683ca5b05eb04ea8aa43e942a104095
Khometa <pavel.khometa@emc.com>
2016-09-13 14:41:15 +0300

    session class created

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,10 +13,10 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login( username="admin", password="secret")
+    app.session.login( username="admin", password="secret")
     app.create_contact(Contact(firstname="first_name", lastName="Last_name", midName="Mid_name", nickName="nick_name",
                             companyName="company1", address1="address11", telHome="homet", telMobile="mobileT",
                             telWork="wrokT", fax="fas", email_1="email11", email_2="email2", email_3="email3",
                             address_2="address2", home_2="home2", note_2="note2", home_page="homePage"))
-    app.logout()
+    app.session.logout()
 
