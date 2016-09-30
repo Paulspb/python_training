dd0ff58ccab73c8de4b9f7df76b7805a69034c87
Khometa <pavel.khometa@emc.com>
2016-09-30 22:36:06 +0300

    after lesson 6.4 ( created group.json & contact.json

---
+++ generator/contact.py
@@ -0,0 +1,52 @@
+from model.contact import Contact
+import random
+import string
+import os.path
+import json
+import getopt
+import sys
+
+
+# from official doc
+# n:      count of generated data
+# n:      file name where it need to put
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of goups", "file ="])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f =  "data/contact.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+
+def random_String(prefix,maxlen):
+    symbol = string.ascii_letters + string.digits + " " *10
+    return prefix + "".join([random.choice(symbol) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname="first2name", lastName="Last2name", midName="Mid2name", nickName="nick2name",
+                    companyName="company12", address1="address112", telHome="homet2", telMobile="mobileT2",
+                    telWork="wrokT2")] + [
+            Contact(firstname=random_String("first_name",10), lastName=random_String("Last_name",20),
+                               companyName=random_String("company1",20), address1=random_String("address11",20),
+                                telHome=random_String("homet",20), telMobile=random_String("mobileT",10),telWork=random_String("wrokT",10),
+                                email_1=random_String("email11",10), email_2=random_String("email2",10), email_3=random_String("email3",10),
+                                home_2=random_String("home2",10))
+            #for i in range (5)
+            for i in range (n)
+]
+
+
+#file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/contact.json")
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)),"..", f)
+
+with open(file, "w") as out:
+    #f.writable(json.dumps(testdata))
+    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
---
+++ generator/group.py
@@ -0,0 +1,52 @@
+from model.group import Group
+import random
+import string
+import os.path
+import json
+import getopt
+import sys
+
+
+# from official doc
+# n:      count of generated data
+# n:      file name where it need to put
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of goups", "file ="])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f =  "data/groups.json"
+# script paramater could be fill-in   -n 6 if data\test_group.json
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+
+def random_String(prefix,maxlen):
+    symbol = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbol) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Group(name="", header="", footer="")] + [
+      Group(name=random_String("name",10),header=random_String("header",20),footer=random_String("footer",15))
+      # for i in range (5)
+      for i in range(n)
+      # this is 5 time for random  + one 'empty'
+]
+
+# define path file  ../ as file defined into -generator- dir
+# #file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/group.json")
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+#define writubng to file  , where .dumps  convert structure to json-format
+with open(file, "w") as out:
+    #f.writable(json.dumps(testdata))
+        # it need to clarify how to convert Group to json where __dict__ keeps all __init__
+    #    f.write(json.dumps(testdata, default=lambda x: x.__dict__))
+        # now split one line to several lines
+    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
