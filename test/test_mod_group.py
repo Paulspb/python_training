62e4c8a9c4356f813073c450e38357500af6d83f
Khometa <pavel.khometa@emc.com>
2016-10-10 15:34:45 +0300

    update mod_geoup_test

--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -8,8 +8,6 @@ def test_mod_group(app, db):
             #less 7.4old_groups = app.group.get_group_list()
     old_groups = db.get_group_list()
             #less 7.4 index = randrange(len(old_groups))
-    index = randrange(len(old_groups))
-
     group_random = random.choice(old_groups)
     group = Group(name="group1mod", header="group1Hmod", footer="group1Fmod")
             # less 7.4 group.id = old_groups[index].id
@@ -18,24 +16,35 @@ def test_mod_group(app, db):
     app.group.modify_group_by_id(group_random.id, group)
             #less 7.4 new_groups = app.group.get_group_list()
     new_groups = db.get_group_list()
-    assert len(old_groups)  == len(new_groups)
+            #home 20
+    old_groups.remove(group_random)
+    old_groups.append(group)
+    #assert len(old_groups)  == len(new_groups)
         ###old_groups.choice(group_random.id) = group
-    old_groups[index] = group
+        ##old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_mod_group_name(app):
+def test_mod_group_name(app,db):
     if app.group.count() == 0:
         app.group.create(Group(name="add1"))
     old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
+    # less 7.4 index = randrange(len(old_groups))
+    group_random = random.choice(old_groups)
     group =  Group(name="group1mod")
-    group.id = old_groups[0].id
-    app.group.modify_first_group(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
-    #old_groups[index] = group
-    #assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    # less 7.4 group.id = old_groups[0].id
+    group.id = group_random.id
+    app.group.modify_group_by_id(group_random.id, group)
+    # less 7.4 new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
+    # home 20
+    old_groups.remove(group_random)
+    old_groups.append(group)
+    # assert len(old_groups)  == len(new_groups)
+    ###old_groups.choice(group_random.id) = group
+    ##old_groups[index] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
 
 def test_mod_group_header(app):
     if app.group.count() == 0:
